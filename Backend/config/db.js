051e382a366151c9217fe884f16bcb374e4714b8
techwizard31 <istaprasad.patra@gmail.com>
2024-01-09 17:18:36 +0530

    11

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose")
 const dotenv = require("dotenv");
 dotenv.config();
-const uri = process.env.MONGO_URI
 
 const connectDB = async()=>{
     try{
-         const conn = await mongoose.connect(uri,{
+         const conn = await mongoose.connect(`${process.env.MONGO_URI}`,{
              useUnifiedTopology:true,
             useNewUrlParser:true,
             useCreateIndex: true
