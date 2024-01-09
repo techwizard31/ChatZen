fa8a3c9aec3a488a0d8f78590f21ec458b6718a1
techwizard31 <istaprasad.patra@gmail.com>
2024-01-09 18:18:37 +0530

    14

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,10 +4,7 @@ dotenv.config();
 
 const connectDB = async()=>{
     try{
-         const conn = await mongoose.connect(`${process.env.MONGO_URI}`,{
-             useUnifiedTopology:true,
-            useNewUrlParser:true,
-         })
+         const conn = await mongoose.connect(`${process.env.MONGO_URI}`)
          console.log(`MongoDB Connected: ${conn.connection.host}`);
     }catch(error){
         console.log(`Error: ${error.message}`);
