3c491509a530d53e96c87423357db813ed68b58d
techwizard31 <istaprasad.patra@gmail.com>
2024-08-07 23:52:33 +0530

    recent

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,7 +4,10 @@ dotenv.config();
 
 const connectDB = async()=>{
     try{
-         const conn = await mongoose.connect(`${process.env.MONGO_URI}`)
+         const conn = await mongoose.connect(process.env.MONGO_URI,{
+             useUnifiedTopology:true,
+            useNewUrlParser:true,
+         })
          console.log(`MongoDB Connected: ${conn.connection.host}`);
     }catch(error){
         console.log(`Error: ${error.message}`);
--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
-
+  
 app.use(notFound);
 app.use(errorHandler);
 
